7e73db68c7f6cd38338f71d0a2f792eb382a29dd
MITHUN B PALAT <mithunbpalat@gmail.com>
2021-12-17 14:23:57 +0530

    Add files via upload

---
+++ MessageResponse.java
@@ -0,0 +1,22 @@
+package com.app.payload.response;
+
+public class MessageResponse {
+	public MessageResponse() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	private String message;
+
+	
+	public MessageResponse(String message) {
+		super();
+		this.message = message;
+	}
+	
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
---
+++ Role.java
@@ -0,0 +1,51 @@
+package com.app.models;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Enumerated(EnumType.STRING)
+	@Column(length = 20)
+	private ERole name;
+
+	
+
+	public Role() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+
+	public Role(Integer id, ERole name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+
+	public Role(ERole name) {
+		this.name = name;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public ERole getName() {
+		return name;
+	}
+
+	public void setName(ERole name) {
+		this.name = name;
+	}
+}
